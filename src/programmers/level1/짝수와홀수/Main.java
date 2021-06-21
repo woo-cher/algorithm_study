9a15d4e605c9a83ffa1d7e0b54dcbab5ca03ce1e
lminggvick <lminggvick@naver.com>
2021-06-21 15:03:09 +0900

    짝수와홀수

---
+++ src/programmers/level1/짝수와홀수/Main.java
@@ -0,0 +1,15 @@
+package programmers.level1.짝수와홀수;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        }
+
+        return "Odd";
+    }
+}

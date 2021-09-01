eb6647749a45828201ceaa76a60a2038da1d551f
lminggvick <lminggvick@naver.com>
2021-09-01 16:11:10 +0900

    위클리 챌린지 1주차

---
+++ src/programmers/level1/위클리챌린지/Main1.java
@@ -0,0 +1,21 @@
+package programmers.level1.위클리챌린지;
+
+// 위클리 챌린지 1주차
+public class Main1 {
+    public static long solution(int price, int money, int count) {
+        long answer = -1;
+        long sum = 0;
+
+        for (int i = 1; i < count + 1; i++) {
+            sum += (long) price * i;
+        }
+
+        answer = sum - money;
+
+        return answer < 0 ? 0 : answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 20, 4));
+    }
+}

4668261281bda835ecb64468cf058b1bc0aa83d2
lminggvick <lminggvick@naver.com>
2021-10-27 18:37:52 +0900

    주식가격 2번째 풀이 추가

--- src/programmers/level2/주식가격/Main.java
+++ src/programmers/level2/주식가격/Main.java
@@ -14,14 +14,13 @@ public class Main {
         }
 
         // loop
-
         int n = 0;
         int idx = answer.length - 1;
 
         while (!stack.isEmpty()) {
             int now = stack.pop();
-
             int sec = 0;
+
             for (int i = prices.length - n; i < prices.length; i++) {
                 sec++;
 
@@ -37,7 +36,28 @@ public class Main {
         return answer;
     }
 
+    public static int[] bruteForce(int[] prices) {
+        int[] answer = new int[prices.length];
+        int size = prices.length;
+
+        for (int i = 0; i < size; i++) {
+            int sec = 0;
+
+            for (int j = i + 1; j < size; j++) {
+                sec++;
+
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+
+            answer[i] = sec;
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 2, 3})));
+        System.out.println(Arrays.toString(bruteForce(new int[]{1, 2, 3, 2, 3})));
     }
 }
